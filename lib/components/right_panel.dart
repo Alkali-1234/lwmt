692835fc775dcae937f76e36f2596216020c2007
Alkali-1234 <algazel.faizun@outlook.com>
2026-01-29 11:01:16 +0700

    Added auto and alloc config

--- lib/components/right_panel.dart
+++ lib/components/right_panel.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lwmt/components/calendar_date.dart';
+import 'package:lwmt/components/cursor_fedback.dart';
+import 'package:lwmt/screens/home_state.dart';
 
 class RightPanel extends ConsumerStatefulWidget {
   const RightPanel({super.key});
@@ -39,7 +42,177 @@ class _RightPanelState extends ConsumerState<RightPanel> {
             ),
           ),
           const SizedBox(height: 16),
+          //* Auto & Budget
+          Row(
+            children: [
+              CursorFedback(
+                child: ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 16,
+                    ),
+                    backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadiusGeometry.circular(10),
+                    ),
+                  ),
+                  onPressed: () => showDialog(
+                    context: context,
+                    builder: (c) => SetAutoModal(),
+                  ),
+                  label: Text(
+                    "Auto",
+                    style: TextStyle(color: Colors.black, fontSize: 16),
+                  ),
+                  icon: Icon(Icons.add_rounded, color: Colors.black, size: 20),
+                ),
+              ),
+              const SizedBox(width: 16),
+              ConfigBudgetButton(),
+            ],
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class ConfigBudgetButton extends StatefulWidget {
+  const ConfigBudgetButton({super.key});
+
+  @override
+  State<ConfigBudgetButton> createState() => _ConfigBudgetButtonState();
+}
+
+class _ConfigBudgetButtonState extends State<ConfigBudgetButton> {
+  bool hover = false;
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (event) => setState(() => hover = true),
+      onExit: (event) => setState(() => hover = false),
+      child: ElevatedButton.icon(
+        style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+          backgroundColor: hover ? Colors.white : Colors.black,
+          foregroundColor: hover ? Colors.black : Colors.white,
+          side: BorderSide(color: Colors.white, width: 2),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadiusGeometry.circular(10),
+          ),
+        ),
+        onPressed: () => showDialog(
+          context: context,
+          builder: (context) => BudgetSetModal(),
+        ),
+        label: Text("Budget", style: TextStyle(fontSize: 16)),
+        icon: Icon(Icons.settings_rounded, size: 20),
+      ),
+    );
+  }
+}
+
+class BudgetSetModal extends StatefulWidget {
+  const BudgetSetModal({super.key});
+
+  @override
+  State<BudgetSetModal> createState() => _BudgetSetModalState();
+}
+
+class _BudgetSetModalState extends State<BudgetSetModal> {
+  TextEditingController controller = .new();
+  void submit(WidgetRef ref) {
+    if (controller.text.isEmpty) {
+      return;
+    }
+    int? parse = int.tryParse(controller.text);
+    if (parse == null) {
+      return;
+    }
+    var mo = ref.read(currentMonthProvider);
+    if (mo.value == null) return;
+    ref
+        .read(currentMonthProvider.notifier)
+        .updateMonth(mo.value!.copyWith(allocation: parse));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: SizedBox(
+          width: 300,
+          child: Column(
+            mainAxisSize: .min,
+            children: [
+              Text(
+                "Set your budget.",
+                style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 16),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: controller,
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      style: TextStyle(fontSize: 24, color: Colors.white),
+                      decoration: InputDecoration(
+                        hintText: "xxx.xxx.xxx,xx",
+                        hintStyle: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white.withValues(alpha: 0.5),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  Text(
+                    "IDR",
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              Consumer(
+                builder: (context, ref, _) {
+                  return CursorFedback(
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton.icon(
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 16,
+                            vertical: 16,
+                          ),
+                          backgroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadiusGeometry.circular(10),
+                          ),
+                        ),
+                        onPressed: () {
+                          submit(ref);
+                          Navigator.of(context).pop();
+                        },
+                        label: Text(
+                          "Confirm",
+                          style: TextStyle(color: Colors.black, fontSize: 16),
+                        ),
+                        icon: Icon(
+                          Icons.check_rounded,
+                          color: Colors.black,
+                          size: 20,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
@@ -110,6 +283,116 @@ class CalendarWidgetState extends State<CalendarWidget> {
           ],
         ],
       ],
+    );
+  }
+}
+
+class SetAutoModal extends ConsumerStatefulWidget {
+  const SetAutoModal({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _SetAutoModalState();
+}
+
+class _SetAutoModalState extends ConsumerState<SetAutoModal> {
+  TextEditingController controllerValue = .new();
+  TextEditingController controllerInfo = .new();
+  void submit() {
+    if (controllerInfo.text.isEmpty || controllerValue.text.isEmpty) {
+      return;
+    }
+    int? parse = int.tryParse(controllerValue.text);
+    if (parse == null) return;
+    var mo = ref.read(currentMonthProvider).value;
+    if (mo == null) return;
+    for (var day in mo.days) {
+      day.spendings.add((controllerInfo.text, parse));
+    }
+    ref.read(currentMonthProvider.notifier).updateMonth(mo);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: Padding(
+        padding: const EdgeInsetsGeometry.all(32),
+        child: SizedBox(
+          width: 300,
+          child: Column(
+            mainAxisSize: .min,
+            children: [
+              Text(
+                "Set Auto",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 36,
+                ),
+              ),
+              const SizedBox(height: 16),
+              TextField(
+                controller: controllerInfo,
+                style: TextStyle(fontSize: 20),
+                decoration: InputDecoration(
+                  hintText: "item info",
+                  hintStyle: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.white.withValues(alpha: 0.5),
+                  ),
+                  border: OutlineInputBorder(borderSide: BorderSide.none),
+                ),
+              ),
+              const SizedBox(height: 16),
+              TextField(
+                controller: controllerValue,
+                style: TextStyle(fontSize: 20),
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                decoration: InputDecoration(
+                  hintText: "value",
+                  hintStyle: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.white.withValues(alpha: 0.5),
+                  ),
+                  border: OutlineInputBorder(borderSide: BorderSide.none),
+                ),
+              ),
+              const SizedBox(height: 16),
+              CursorFedback(
+                child: SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 16,
+                      ),
+                      backgroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadiusGeometry.circular(10),
+                      ),
+                    ),
+                    onPressed: () {
+                      submit();
+                      Navigator.of(context).pop();
+                    },
+                    label: Text(
+                      "Confirm",
+                      style: TextStyle(color: Colors.black, fontSize: 16),
+                    ),
+                    icon: Icon(
+                      Icons.check_rounded,
+                      color: Colors.black,
+                      size: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
